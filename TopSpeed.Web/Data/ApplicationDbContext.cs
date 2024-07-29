376e19d87ae57afa7bac4617be35d416ad167372
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-29 15:17:54 +0530

    Create the DbContext in Data Folder And pass the model (Brand) to Brands Table

---
+++ TopSpeed.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopSpeed.Web.Models;
+
+namespace TopSpeed.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
