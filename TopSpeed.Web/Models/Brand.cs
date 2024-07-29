1e29d7e54548c2509c16ba1f91cc10d858498f37
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-29 14:52:50 +0530

    Add project files.

---
+++ TopSpeed.Web/Models/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopSpeed.Web.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid Id  { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int EstablishedYear { get; set; }
+
+        public string BrandLogo { get; set; }
+    }
+}
