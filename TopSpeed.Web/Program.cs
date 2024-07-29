42630725f14ce8ca198c8fc1733aefe32c58135b
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-29 15:20:11 +0530

    Update the porgram.cs to add the database details to access the database in entire project

--- TopSpeed.Web/Program.cs
+++ TopSpeed.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TopSpeed.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
